c88b4a93e95f66e93c35f3026a5ac9fd4a6f6933
Mehmet Palaz <m.palaz@papara.com>
2025-07-13 14:32:22 +0300

    feat: add POST api/transfers endpoint with MediatR

---
+++ TransferService/TransferService.API/Controllers/TransfersController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TransferService.Application.Commands.CreateTransfer;
+
+namespace TransferService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransfersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TransfersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTransfer([FromBody] CreateTransferCommand command)
+        {
+            var transferId = await _mediator.Send(command);
+            
+            return Ok(new { TransferId = transferId });
+        }
+    }
+}
--- TransferService/TransferService.API/Program.cs
+++ TransferService/TransferService.API/Program.cs
@@ -1,6 +1,13 @@
+using TransferService.Application.Commands.CreateTransfer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(CreateTransferCommand).Assembly);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
